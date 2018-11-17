245f58f24ac5e8d2fae48e0b61cbe09233fd0bbd
IncoCode <akulich.dimon@gmail.com>
2018-11-17 17:56:04 +0200

    Init

--- Code/RemoteKeylessSystemForCar/RemoteKeylessSystem.cs
+++ Code/RemoteKeylessSystemForCar/RemoteKeylessSystem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GTA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace RemoteKeylessSystemForCar
 {
-    public class Class1
+    public class RemoteKeylessSystem : Script
     {
     }
 }
