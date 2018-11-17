80e7710f31f1881e33eb2df56d0bba1886b047cd
IncoCode <akulich.dimon@gmail.com>
2018-11-17 19:17:52 +0200

    Add smart lock model

---
+++ Code/RemoteKeylessSystemForCar/Model/VehicleSmartLock.cs
@@ -0,0 +1,61 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteKeylessSystemForCar.Model
+{
+    internal class VehicleSmartLock
+    {
+        private readonly Vehicle _vehicle;
+        private bool _isLocked;
+        private bool _isAutoLockHasBeenActivated; // ToDo: delete, move to controller
+
+        public VehicleSmartLock(Vehicle vehicle)
+        {
+            this._vehicle = vehicle;
+            this._isLocked = false;
+            this._isAutoLockHasBeenActivated = false;
+        }
+
+        public void Lock()
+        {
+            if (this._isLocked)
+            {
+                return;
+            }
+
+            this._isLocked = true;
+
+            this._vehicle.LockStatus = VehicleLockStatus.Locked;
+            this._vehicle.SoundHorn(300);
+            this._vehicle.LightsOn = true;
+            Script.Wait(300);
+            this._vehicle.LightsOn = false;
+            this._vehicle.EngineRunning = false;
+        }
+
+        public void Unlock()
+        {
+            if (!this._isLocked)
+            {
+                return;
+            }
+
+            this._vehicle.LockStatus = VehicleLockStatus.Unlocked;
+            for (int i = 0; i < 2; i++)
+            {
+                this._vehicle.SoundHorn(300);
+                this._vehicle.LightsOn = true;
+                Script.Wait(300);
+                this._vehicle.LightsOn = false;
+                Script.Wait(100);
+            }
+
+            this._isLocked = false;
+            this._vehicle.EngineRunning = false;
+        }
+    }
+}
--- Code/RemoteKeylessSystemForCar/RemoteKeylessSystem.cs
+++ Code/RemoteKeylessSystemForCar/RemoteKeylessSystem.cs
@@ -1,13 +1,80 @@
 ﻿using GTA;
+using System.Windows.Forms;
+using GTA.Native;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RemoteKeylessSystemForCar.Model;
 
 namespace RemoteKeylessSystemForCar
 {
     public class RemoteKeylessSystem : Script
     {
+        private VehicleSmartLock vl;
+
+        public RemoteKeylessSystem()
+        {
+            this.Tick += this.RemoteKeylessSystem_Tick;
+            this.KeyDown += this.RemoteKeylessSystem_KeyDown;
+
+            this.vl = new VehicleSmartLock(Game.Player.Character.LastVehicle);
+        }
+
+        private void RemoteKeylessSystem_Tick(object sender, EventArgs e)
+        {
+            //Vehicle lastVehicle = Game.Player.Character.LastVehicle;
+            //if (!lastVehicle.IsInRangeOf(Game.Player.Character.Position, 6f))
+            //{
+            //    if (lastVehicle.LockStatus == VehicleLockStatus.Locked)
+            //    {
+            //        return;
+            //    }
+            //    lastVehicle.LockStatus = VehicleLockStatus.Locked;
+            //    UI.ShowSubtitle("Vehicle auto locked");
+            //}
+        }
+
+        private void RemoteKeylessSystem_KeyDown(object sender, KeyEventArgs e)
+        {
+#if DEBUG
+            var currentVehicle = Game.Player.Character.CurrentVehicle;
+            var lastVehicle = Game.Player.Character.LastVehicle;
+
+            if (e.KeyCode == Keys.I)
+            {
+                if (currentVehicle == null)
+                {
+                    return;
+                }
+
+                currentVehicle.HasAlarm = true;
+                UI.ShowSubtitle("Alarm has been set up");
+            }
+            else if (e.KeyCode == Keys.K)
+            {
+                if (lastVehicle == null)
+                {
+                    return;
+                }
+
+                //lastVehicle.LockStatus = VehicleLockStatus.Locked;
+                vl.Lock();
+                UI.ShowSubtitle("Vehicle locked");
+            }
+            else if (e.KeyCode == Keys.O)
+            {
+                if (lastVehicle == null)
+                {
+                    return;
+                }
+
+                //lastVehicle.LockStatus = VehicleLockStatus.Unlocked;
+                vl.Unlock();
+                UI.ShowSubtitle("Vehicle unlocked");
+            }
+#endif
+        }
     }
 }
